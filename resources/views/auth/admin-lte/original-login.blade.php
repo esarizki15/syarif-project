a0b90ffab4210b036ab0919e81c96a65f2243847
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2020-11-09 11:49:34 +0700

    add admin-lte sample page

--- resources/views/auth/admin-lte/original-login.blade.php
+++ resources/views/auth/admin-lte/original-login.blade.php
@@ -2,18 +2,16 @@
 @section('content')
 <div class="login-box">
   <div class="login-logo">
-    {{-- <a href="/"><img src="{{ asset('img/ahass.png') }}" style="max-width: -webkit-fill-available;"></a> --}}
-    <a href="/"><b>AdminLTE</b></a>
+    <a href="../../index2.html"><b>Admin</b>LTE</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form method="POST" action="{{ route('login') }}">
-        @csrf
+      <form action="../../index3.html" method="post">
         <div class="input-group mb-3">
-          <input type="email" name="email" required class="form-control" placeholder="Email">
+          <input type="email" class="form-control" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -21,7 +19,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" required placeholder="Password">
+          <input type="password" class="form-control" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -31,7 +29,7 @@
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
-              <input type="checkbox" name="remember" id="remember">
+              <input type="checkbox" id="remember">
               <label for="remember">
                 Remember Me
               </label>
@@ -45,9 +43,23 @@
         </div>
       </form>
 
-      {{-- <p class="mb-0">
-        <a href="/register" class="text-center">Register a new membership</a>
-      </p> --}}
+      <div class="social-auth-links text-center mb-3">
+        <p>- OR -</p>
+        <a href="#" class="btn btn-block btn-primary">
+          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+        </a>
+        <a href="#" class="btn btn-block btn-danger">
+          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+        </a>
+      </div>
+      <!-- /.social-auth-links -->
+
+      <p class="mb-1">
+        <a href="forgot-password.html">I forgot my password</a>
+      </p>
+      <p class="mb-0">
+        <a href="register.html" class="text-center">Register a new membership</a>
+      </p>
     </div>
     <!-- /.login-card-body -->
   </div>
