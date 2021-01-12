ca3fde21aae51c5a291380397185a86d0138867d
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2021-01-12 12:18:30 +0700

    up

--- resources/views/guru/index.blade.php
+++ resources/views/guru/index.blade.php
@@ -20,22 +20,27 @@
                 <table class="table table-hover display nowrap" style="width:100%">
                     <thead>
                         <tr>
+                            <th scope="col">Foto</th>
                             <th scope="col">NIP</th>
                             <th scope="col">Nama</th>
                             <th scope="col">Jenis Kelamin</th>
                             <th scope="col">Mata Pelajaran</th>
                             <th scope="col">Email</th>
+                            <th scope="col">No Telp</th>
                             <th scope="col">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($guru as $data)
                             <tr>
+                                <td><img src="{{ asset('img/' . $data->foto) }}" alt="none" style="max-width: -webkit-fill-available;
+                                    max-height: 200px;"></td>
                                 <td>{{ strtoupper($data->nip) }}</td>
                                 <td>{{ strtoupper($data->nama) }}</td>
                                 <td>{{ strtoupper($data->jenis_kelamin) }}</td>
                                 <td>{{ strtoupper($data->mapel->name) }}</td>
                                 <td>{{ $data->email }}</td>
+                                <td>{{ $data->hp }}</td>
                                 <td>
                                     @include('partial.action', ['data' => $data, 'route'=>'guru'])
                                 </td>
