07b1d937e5c5acf71b84caa703039f6ae61a73ea
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2021-01-17 12:56:01 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,66 +2,11 @@
 
 @section('content')
 <div class="container">
+    @if(Auth::user()->role_id == 1 || Auth::user()->role_id == 5)
+    @include('partial.dashboard_info')
+    @endif
     <div class="row">
-        <div class="col-lg-3 col-6">
-            <div class="small-box bg-info">
-                <div class="inner">
-                <h3>{{ App\User::where('role_id', 2)->count() }}</h3>
-
-                <p>Siswa</p>
-                </div>
-                <div class="icon">
-                <i class="ion ion-person"></i>
-                </div>
-                @if(Auth::user()->role_id == 1)
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                @endif
-            </div>
-        </div>
-        <div class="col-lg-3 col-6">
-            <div class="small-box bg-success">
-                <div class="inner">
-                <h3>{{ App\User::where('role_id', 3)->count() }}</h3>
-
-                <p>Guru</p>
-                </div>
-                <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-                </div>
-                @if(Auth::user()->role_id == 1)
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                @endif
-            </div>
-        </div>
-        <div class="col-lg-3 col-6">
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>{{ App\Kelas::count() }}</h3>
-                <p>Kelas</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              @if(Auth::user()->role_id == 1)
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-              @endif
-            </div>
-          </div>
-          <div class="col-lg-3 col-6">
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>4</h3>
-
-                <p>Mapel</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              @if(Auth::user()->role_id == 1)
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-              @endif
-            </div>
-          </div>
+        <div class="col"></div>
     </div>
     @forelse ($pengumuman as $data)
     <div class="row justify-content-center">
