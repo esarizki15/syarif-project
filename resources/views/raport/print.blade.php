bab47a6f120988bdf8b1a275aa05e65d4c3b2b3d
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2021-01-18 07:40:12 +0700

    up

--- resources/views/raport/print.blade.php
+++ resources/views/raport/print.blade.php
@@ -7,9 +7,9 @@
           {{-- <br><br><br> --}}
           <center>
               <div class="row">
-                {{-- <div>
-                    <img src="{{url('/img/logo_kop.png')}}" alt="..."  style="position:absolute;z-index:1; left:-30px; height:600px;">
-                </div> --}}
+                <div>
+                    <img src="{{url('/img/logo_0.png')}}" alt="..."  style="position:absolute;z-index:1;  height:170px;">
+                </div>
                   <div class="col">
                       <h3>YAYASAN PENDIDIKAN AL HUSNA TANGERANG</h3>
                       <h3><b>MADRASAH TSANAWIYAH AL HUSNA TANGERANG</b></h3>
